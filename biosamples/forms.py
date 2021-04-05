a52642c84f16d5617a135b4dbdd3036470df75d7
caiprozect <caiprozect@gmail.com>
2021-04-05 21:57:51 +0900

    Add complete fields of biosample to model and forms

--- biosamples/forms.py
+++ biosamples/forms.py
@@ -3,8 +3,18 @@ from .models import Biosample, Similarity
 import random
 
 class BiosampleForm(forms.Form):
-    sample_name = forms.CharField()
-    
+    sample_name = forms.CharField(max_length=8)
+    organism = forms.CharField(max_length=32)
+    biosample = forms.CharField(max_length=32)
+    condition = forms.CharField(max_length=255, required=False)
+    treatment = forms.CharField(max_length=255, required=False)
+    treatment_time = forms.CharField(max_length=32, required=False)
+    treatment_conc = forms.CharField(max_length=32, required=False)
+    target = forms.CharField(max_length=32, required=False)
+    assay = forms.CharField(max_length=32, required=False)
+    layout = forms.CharField(max_length=32, required=False)
+    platform = forms.CharField(max_length=32, required=False)
+
     def send_mail(self):
         pass 
     
@@ -16,5 +26,14 @@ class BiosampleForm(forms.Form):
             Similarity.objects.create(src=other, dst=self.biosample, score=score)
 
     def save(self):
-        self.biosample = Biosample.create_new(self.cleaned_data['sample_name'])
+        self.biosample = Biosample.create_new(self.cleaned_data['sample_name'],
+                                            self.cleaned_data['organism'],
+                                            self.cleaned_data['condition'],
+                                            self.cleaned_data['treatment'],
+                                            self.cleaned_data['treatment_time'],
+                                            self.cleaned_data['treatment_conc'],
+                                            self.cleaned_data['target'],
+                                            self.cleaned_data['assay'],
+                                            self.cleaned_data['layout'],
+                                            self.cleaned_data['platform'])
         return self.biosample
--- biosamples/models.py
+++ biosamples/models.py
@@ -2,8 +2,21 @@ import datetime
 from django.db import models
 from django.urls import reverse
 
+import datetime
+
 class Biosample(models.Model):
-    sample_name = models.CharField(max_length=50)
+    sample_name = models.CharField(max_length=8)
+    organism = models.CharField(default='', max_length=32)
+    biosample = models.CharField(default='', max_length=32)
+    condition = models.CharField(max_length=255, blank=True)
+    treatment = models.CharField(max_length=255, blank=True)
+    treatment_time = models.CharField(max_length=32, blank=True)
+    treatment_conc = models.CharField(max_length=32, blank=True)
+    target = models.CharField(max_length=32, blank=True)
+    assay = models.CharField(max_length=32, blank=True)
+    layout = models.CharField(max_length=32, blank=True)
+    platform = models.CharField(max_length=32, blank=True)
+    date = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.sample_name
@@ -12,8 +25,19 @@ class Biosample(models.Model):
         return reverse('biosamples:detail', kwargs={'pk': self.pk})
 
     @staticmethod
-    def create_new(sample_name):
-        biosample = Biosample.objects.create(sample_name=sample_name)
+    def create_new(sample_name, organism, biosample, condition, treatment,
+                treatment_time, treatment_conc, target, assay, layout, platform):
+        biosample = Biosample.objects.create(sample_name=sample_name,
+                                            organism=organism,
+                                            biosample=biosample,
+                                            condition=condition,
+                                            treatment=treatment,
+                                            treatment_time=treatment_time,
+                                            treatment_conc=treatment_conc,
+                                            target=target,
+                                            assay=assay,
+                                            layout=layout,
+                                            platform=platform)
         return biosample 
 
 class Similarity(models.Model):
